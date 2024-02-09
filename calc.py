facc9fb8e668c0ab53d0fb64342a9534fa735047
jyothikatakam396 <84368383+jyothikatakam396@users.noreply.github.com>
2024-02-09 11:42:23 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -5,9 +5,8 @@ def subtract(num1, num2):
     return num1 - num2
 def Multiply(num1, num2):
     return num1*num2
-def Divide(num1, num2):
-    return num1//num2
+
 print(add(2,3))
 print(subtract(5,2))
 print(Multiply(2*3))
-print(Divide(10,5))
+
